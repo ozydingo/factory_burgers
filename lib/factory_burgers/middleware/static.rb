4312046c55a753d70d1b1df33f24f6f129c482ee
Andrew Schwartz <ozydingo@gmail.com>
2021-05-20 00:02:08 -0400

    Add redirect to static_app to fix requests that don't end in /

--- lib/factory_burgers/middleware/static.rb
+++ lib/factory_burgers/middleware/static.rb
@@ -2,7 +2,21 @@ module FactoryBurgers
   module Middleware
     class Static
       def call(env)
+        return slashpath_redirect(env["REQUEST_PATH"]) if slashpath_redirect?(env)
+
         rack_static.call(env)
+      end
+
+      def slashpath_redirect?(env)
+        env["REQUEST_PATH"] == env["SCRIPT_NAME"] && env["REQUEST_PATH"][-1] != "/"
+      end
+
+      def slashpath_redirect(path)
+        return [
+          302,
+          {'Location' => path + "/", 'Content-Type' => 'text/html', 'Content-Length' => '0'},
+          [],
+        ]
       end
 
       def rack_static
